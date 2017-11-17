d0b4252a1d06283e92d3285c88e424336ff7f32d
Sondre Stafsnes <sondre.stafs@gmail.com>
2017-11-17 14:09:52 +0100

    Update comments in spi.c

--- bygg/driver/CAN/spi.c
+++ bygg/driver/CAN/spi.c
@@ -10,19 +10,20 @@
 #include <stdio.h>
 
 void spi_init(){
-	
-	// Bit 7 definera SCK og Bit 6 definera Master Output Slave Input. (Kanskje bytte ut 6 med 5?)
+
+	// Bit 7 definera SCK og Bit 6 definera Master Output Slave Input.
+
 	DDRB = (1 << DDB7)|(1 << DDB5)|(1 << DDB4);
 	DDRB &= ~(1 << DDB6);
 	/*SPDR: SPI Data Register */
 	/*SPSR: SPI Status Register */
 	/*SPCR:	SPI Control Register */
 
-	SPCR |= (1 << MSTR);	// Vel vår SPI som Storbonde over den andre Drengen. AKA Høvding/Trell
-	SPCR |= (1 << SPR0);	// Set antal klokkeslag for klokkesignalet som skal sendast. SPR0 = 1 gir Fosc/16.
-	SPCR |= (1 << SPE);		// Aktiver SPI Grensesnitt
-	
-	
+	SPCR |= (1 << MSTR);	// Define our microchip as master and the other as slave in the SPI interface.
+	SPCR |= (1 << SPR0);	// Define clock pulses.
+	SPCR |= (1 << SPE);		// Activate SPI interface.
+
+
 	/*
 	SPCR bit definitions
 
@@ -35,35 +36,36 @@ void spi_init(){
 	#define SPR1	1
 	#define SPR0	0
 	*/
-	
+
 }
 
 uint8_t spi_send(uint8_t cData){
 
-	
+	// Output data in the Data Register.
 	SPDR = cData;
 	//Wait for transmission to complete
 	while(!(SPSR & (1<<SPIF))){}
-	
+
 }
 
 uint8_t spi_read(){ //When master sends information, it sends a dummy bit first!
 
-	spi_send(0xFF); //dummy byte
+	spi_send(0xFF); // Dummy byte, we need to send data to the MCP to read data with SPI.
 
 	//uint8_t *data = 1;
 	//SPDR = data;
 	//Wait for reception to complete
 	while(!(SPSR & (1<<(SPIF)))){}
-	
+
 	return SPDR;
-	
+
 }
 
-void spi_chipselect(uint8_t select){
+void spi_chipselect(uint8_t select)     // Chip select.
+{
 	if (select == 0){
 		PORTB |= (1<<PB4);
-	} 
+	}
 	else{
 		PORTB &= ~(1<<PB4);
 	}
